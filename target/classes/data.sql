c957b482cc16d26a5998ea7ebe5d70d53a0393c5
Corey Prideaux <cpride67@protonmail.com>
2020-06-09 20:15:00 -0500

    Revert
    Removed changes for add full name and location.

--- target/classes/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO 
-	TBL_EMPLOYEES (vol_full_name, vol_location, vol_email) 
-VALUES
-  	('full name', 'location', 'howtodoinjava@gmail.com'),
-  	('full name', 'location', 'xyz@email.com');
