127542a49f1f46b014ddcb4b2351010c7420a780
aredondo <aredondo@Austins-MacBook-Pro.local>
2020-06-18 11:29:05 -0500

    Changed welcome comment in a new branch

--- demo/app.groovy
+++ demo/app.groovy
@@ -3,7 +3,7 @@ class ThisWillActuallyRun {
 
     @RequestMapping("/")
     String home() {
-        "Hello and welcome to volunteeRocks App! Brought to you by Abit, Austin, and Corey!"
+        "Hello and welcome to the volunteeRocks App! Created by Abit, Austin, and Corey!"
     }
 
 }
