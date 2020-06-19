542e4ce0e72a527799f06d34b39c668f730d2adc
aabit <abitabuoi@gmail.com>
2020-06-18 20:51:23 -0500

    restructure folders

--- demo/app.groovy
+++
@@ -1,9 +0,0 @@
-@RestController
-class ThisWillActuallyRun {
-
-    @RequestMapping("/")
-    String home() {
-        "Hello and welcome to volunteeRocks App! Brought to you by Abit, Austin, and Corey!"
-    }
-
-}
--- demo/helloWorld/app.groovy
+++ demo/helloWorld/app.groovy
@@ -3,7 +3,7 @@ class ThisWillActuallyRun {
 
     @RequestMapping("/")
     String home() {
-        "Hello and welcome to volunteeRocks App!"
+        "Hello and welcome to volunteeRocks App! Proudly brought to you by Abit, Austin and Corey"
     }
 
 }
